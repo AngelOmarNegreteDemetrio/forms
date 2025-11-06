04fe9bcc4ca25bccad5a5bba5a645585c2fc99ec
AngelOmarNegreteDemetrio <negretedemetrioangel@gmail.com>
2025-11-06 15:24:38 -0600

    avatar completado

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './navigation/AppNavigator';
 
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
